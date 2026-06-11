779aae39de8fce42939b0d453f03cd02065ad354
Kamal Bhaskar <kamalbhaskar56736@gmail.com>
2026-06-11 09:40:00 +0530

    canva

---
+++ canvas.py
@@ -0,0 +1,16 @@
+from tkinter import*
+root = Tk()
+
+canvas_width = 800
+canvas_height =400
+root.geometry(f"{canvas_width}x{canvas_height}")
+root.title("hello kamal")
+can_widget = Canvas(root,width = canvas_width,height=canvas_height)
+can_widget.pack()
+#the line goes from the point x1,y1,x2,y2
+can_widget.create_line(0,0,700,500,fill="red")
+can_widget.create_line(0,500,900,0,fill="red")
+#can_widget.create_rectangle(3,5,700,300,fill="blue")
+can_widget.create_text(200,400,text="kamal bhaskar")
+
+root.mainloop()
